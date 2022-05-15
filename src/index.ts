cd9751515bfbf1404de303e2442e4eacfac91bbe
robinleboe-sw <robin@sessionwire.com>
2022-05-14 20:13:13 -0700

    add CharactersCollection

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { GenericSort } from './GenericSort';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([11, 2, -3, 0]);
-const arraySort = new GenericSort(numbersCollection);
-arraySort.sort();
-console.log(numbersCollection.items);
+// const numbersCollection = new NumbersCollection([11, 2, -3, 0]);
+// const arraySort = new GenericSort(numbersCollection);
+// arraySort.sort();
+// console.log(numbersCollection.items);
+
+const charactersCollection = new CharactersCollection('abacab');
+const charSort = new GenericSort(charactersCollection);
+charSort.sort();
+console.log(charactersCollection.chars);
