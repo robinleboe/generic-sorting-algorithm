22f3da8b189f7f2c7f6d69ced0ddb111b0fb77c0
robinleboe-sw <robin@sessionwire.com>
2022-05-14 17:34:39 -0700

    basic bubble sort implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('✅ testing concurrent build and execution...');
+// class ArraySort {
+//   items: number[];
+
+//   constructor(items: number[]) {
+//     this.items = items;
+//   }
+// }
+
+// refactor as public
+class ArraySort {
+  constructor(public items: number[]) {}
+
+  sort(): void {
+    const { length } = this.items;
+    // simple bubble sort
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.items[j] > this.items[j + 1]) {
+          const leftHandVal = this.items[j];
+          this.items[j] = this.items[j + 1];
+          this.items[j + 1] = leftHandVal;
+        }
+      }
+    }
+  }
+}
+
+const arraySort = new ArraySort([11, 2, -3, 0]);
+arraySort.sort();
+console.log(arraySort.items);
