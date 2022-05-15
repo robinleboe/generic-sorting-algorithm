55c5a6ff363715dfff70fb01673b7d7c81ab366c
robinleboe-sw <robin@sessionwire.com>
2022-05-14 21:24:51 -0700

    add Node and LinkedList classes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,24 @@
 import { GenericSort } from './GenericSort';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // const numbersCollection = new NumbersCollection([11, 2, -3, 0]);
 // const arraySort = new GenericSort(numbersCollection);
 // arraySort.sort();
 // console.log(numbersCollection.items);
 
-const charactersCollection = new CharactersCollection('abacab');
-const charSort = new GenericSort(charactersCollection);
-charSort.sort();
-console.log(charactersCollection.chars);
+// const charactersCollection = new CharactersCollection('XaBacab');
+// const charSort = new GenericSort(charactersCollection);
+// charSort.sort();
+// console.log(charactersCollection.chars);
+
+const linkedList = new LinkedList();
+linkedList.add(395);
+linkedList.add(-11);
+linkedList.add(-2);
+linkedList.add(6);
+
+const listSort = new GenericSort(linkedList);
+listSort.sort();
+linkedList.print();
